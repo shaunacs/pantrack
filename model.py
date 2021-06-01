8f88d221634597b7089979ae6da532b421e1a85b
Shauna Saunders <shaunacsaunders@outlook.com>
2021-06-01 20:51:08 +0000

    Created database relations

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 """Models for PanTrack app"""
 
-import datetime
+from flask import Flask
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+app = Flask(__name__)
+app.secret_key = "dev"
 
 
 class User(db.Model):
@@ -21,10 +24,13 @@ class User(db.Model):
     phone_number = db.Column(db.String(10), nullable=False)
     last_appointment = db.Column(db.DateTime)
     is_admin = db.Column(db.Boolean)
-    user_household_id = db.Column(db.Integer, db.ForeignKey('user_households.user_household_id'))
+    household_id = db.Column(db.Integer, db.ForeignKey('households.household_id'))
+
+    household = db.relationship('Household', backref='user')
+    appointment = db.relationship('Appointment', backref='user')
 
     def __repr__(self):
-        return f'<User user_id={self.user_id} name={fname} {lname}>'
+        return f'<User user_id={self.user_id} name={self.fname} {self.lname}>'
 
 
 class Appointment(db.Model):
@@ -37,6 +43,10 @@ class Appointment(db.Model):
                                 primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     appointment_slot_id = db.Column(db.Integer, db.ForeignKey('appointment_slots.appointment_slot_id'))
+
+    appointment_slot = db.relationship('AppointmentSlot', backref='appointment')
+
+    # user = user associated with appointment
 
     def __repr__(self):
         return f'<Appointment appointment_id={self.appointment_id} user_id={self.user_id} appointment_slot={self.appointment_slot_id}>'
@@ -54,16 +64,18 @@ class AppointmentSlot(db.Model):
     end_time = db.Column(db.DateTime)
     date = db.Column(db.DateTime)
 
+    #appointment = appointment made for the slot
+
     def __repr__(self):
         return f'<AppointmentSlot date={self.date} start_time={self.start_time} end_time={self.end_time}>'
 
 
-class UserHousehold(db.Model):
+class Household(db.Model):
     """Contains information regarding a user's household"""
 
-    __tablename__ = "user_households"
+    __tablename__ = "households"
 
-    user_household_id = db.Column(db.Integer,
+    household_id = db.Column(db.Integer,
                                 autoincrement=True,
                                 primary_key=True)
     num_people = db.Column(db.Integer, nullable=False)
@@ -72,5 +84,24 @@ class UserHousehold(db.Model):
     picking_up_for_another = db.Column(db.Boolean)
     special_requests = db.Column(db.Text)
 
+    # user = a list of users in household
+
     def __repr__(self):
-        return f'<UserHousehold user_household_id={self.user_household_id} num_people={self.num_people}>'
+        return f'<Household household_id={self.household_id} num_people={self.num_people}>'
+
+
+def connect_to_db(app, db_uri='postgresql:///appointments'):
+    """Connect to database"""
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = app
+    db.init_app(app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
