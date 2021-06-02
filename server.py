6406afcd0f9aebb03194162062e29d49e7a00adf
Shauna Saunders <shaunacsaunders@outlook.com>
2021-06-02 20:26:00 +0000

    Created server function to handle session

--- server.py
+++ server.py
@@ -19,14 +19,29 @@ def render_hompeage():
         return redirect('/log-in')
 
 
-@app.route('/log-in', methods=["POST"])
+@app.route('/log-in')
 def render_user_log_in():
     """Renders page for user to log in"""
 
-    # session["username"] = request.args["username"]
-
     return render_template('log_in_page.html')
 
+
+@app.route('/handle-form-session')
+def handle_session_form():
+    """Save session and return to homepage"""
+
+    session['username'] = request.args['username']
+    session['password'] = request.args['password']
+
+    return render_template('homepage.html', session=session)
+
+
+# @app.route('/handle-log-in', methods=["POST"])
+# def handle_login():
+#     """Log user into the application"""
+
+#     username = request.form['username']
+#     password = request.form['password']
 
 
 if __name__ == '__main__':
