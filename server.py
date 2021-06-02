c3a3202e7cdc8ffbf5fd520d98b0a3c5663b1bd0
Shauna Saunders <shaunacsaunders@outlook.com>
2021-06-02 19:43:11 +0000

    Beginning log in page

--- server.py
+++ server.py
@@ -13,7 +13,19 @@ app.jinja_env.undefined = StrictUndefined
 def render_hompeage():
     """Displays the homepage"""
 
-    return render_template('homepage.html')
+    if 'username' in session:
+        return render_template('homepage.html')
+    else:
+        return redirect('/log-in')
+
+
+@app.route('/log-in', methods=["POST"])
+def render_user_log_in():
+    """Renders page for user to log in"""
+
+    # session["username"] = request.args["username"]
+
+    return render_template('log_in_page.html')
 
 
 
