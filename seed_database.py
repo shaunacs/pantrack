97a3e54bd611c7915b47aaa9f1937bc7b412d35b
Shauna Saunders <shaunacsaunders@outlook.com>
2021-06-07 16:50:22 +0000

    Added test appointment slots to seed database

--- seed_database.py
+++ seed_database.py
@@ -1,7 +1,7 @@
 """Script to seed database for PanTrack"""
 
 import os
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta, time
 import crud
 import model
 import server
@@ -23,13 +23,18 @@ for n in range(10):
     user = crud.create_user(fname, lname, email, username, password, phone_number)
 
 # create test appointment slots
-start_date = date(2021, 6, 4)
-end_date = date(2021, 6, 14)
-delta = timedelta(days=1)
+date = date(2021, 6, 4)
+# end_date = date(2021, 6, 14)
+delta = timedelta(minutes=15)
+start_time = datetime(2021, 6, 7, 9, 0)
+end_appts = datetime(2021, 6, 7, 12, 0)
 
-while start_date <= end_date:
-    
-    appt_slot = crud.create_appointment_slot()
+while start_time <= end_appts:
+    end_time = start_time + delta
+    appt_slot = crud.create_appointment_slot(start_time, end_time, date)
+    start_time += delta
+
+
 
 
 
