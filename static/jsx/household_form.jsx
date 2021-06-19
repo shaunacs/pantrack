2bfbf7f1a96a65d4e561e3299602c26bc95e5f1f
Shauna Saunders <shaunacsaunders@outlook.com>
2021-06-19 15:46:57 +0000

    React changes pickup radio button value

--- static/jsx/household_form.jsx
+++ static/jsx/household_form.jsx
@@ -5,11 +5,22 @@
 // }
 
 
-function PickingUpForAnother(props) {
+function PickingUpForAnother() {
     function promptMessage() {
         const pickupName = prompt('Who are you picking up for?');
-        console.log(props.anotherPickup)
         return pickupName
+    }
+
+    const [anotherPickup, handleAnotherPickup] = React.useState('False');
+    const [isHidden, handleHiddenInput] = React.useState("True");
+
+    function truePickup() {
+        handleAnotherPickup('True');
+        handleHiddenInput('False');
+    }
+
+    function falsePickup() {
+        handleAnotherPickup('False');
     }
 
 
@@ -23,10 +34,11 @@ function PickingUpForAnother(props) {
             <input type="radio" name="wants-peanut-butter" value="False"></input>
             <label>No</label><br></br>
             <label>Are you picking up for anyone else?</label>
-            <input onClick={promptMessage} type="radio" name="picking-up-for-another" value="True"></input>
+            <input onClick={truePickup} type="radio" name="picking-up-for-another" value={anotherPickup}></input>
             <label>Yes</label>
-            <input type="radio" name="picking-up-for-another" value="False"></input>
+            <input onClick={falsePickup} type="radio" name="picking-up-for-another" value={anotherPickup}></input>
             <label>No</label><br></br>
+            <input type="text" name="pickup-for" hidden={isHidden}></input>
             <input type="text" name="allergies"></input>
             <label>Allergies</label><br></br>
             <input type="text" name="special-requests"></input>
@@ -37,7 +49,7 @@ function PickingUpForAnother(props) {
 }
 
 ReactDOM.render(
-    <PickingUpForAnother anotherPickup="False"/>,
+    <PickingUpForAnother />,
     document.querySelector('#household-info')
 )
 
